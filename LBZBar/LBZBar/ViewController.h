2af38ef49080ea3bba74c71f25d337d337ed1398
kingly <kingly09@gmail.com>
2015-12-16 16:33:47 +0800

    使用ZBarSDK 需要导入的framework
    1.AVFoundation.framework
    
    2.CoreMedia.framework
    
    3.CoreVideo.framework
    
    4.QuartzCore.framework
    
    5.libiconv.dylib

---
+++ LBZBar/LBZBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBZBar
+//
+//  Created by kingly on 15/12/16.
+//  Copyright © 2015年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
